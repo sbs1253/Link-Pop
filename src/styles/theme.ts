48657a99fbeaa19616dd80458b6a107a47bb2096
SongByeonghun <rokaf340@gmail.com>
2024-08-26 20:45:31 +0900

    Feat: 초기 가이드 설정 - 컬러 스키마, 폰트 사이즈 전역 설정 - reset css 적용 - path alias 설정 - react query 설정 Github issue #1

---
+++ src/styles/theme.ts
@@ -0,0 +1,56 @@
+// theme.js
+
+const theme = {
+  colors: {
+    text: {
+      title: '#FFFFFF',
+      body: '#E0E0E0',
+      bodySubtle: '#A6A6A6',
+      caption: '#8F8F8F',
+    },
+    background: {
+      primary: '#282929',
+      secondary: '#3B3D3D',
+      tertiary: '#4D4F4F',
+    },
+    stroke: {
+      primary: '#353D3D',
+      secondary: '#535252',
+      tertiary: '#707070',
+    },
+    primary: {
+      normal: '#D33F40',
+      hover: '#DC3639',
+      active: '#E11F22',
+      disabled: '#913234',
+      subtle: '#4B1B1F',
+      strong: '#F5C5C5',
+    },
+    success: {
+      normal: '#2EB67D',
+      hover: '#25C285',
+      active: '#1BD96A',
+      disabled: '#1B8356',
+    },
+    information: {
+      normal: '#3269FF',
+      hover: '#4B83FF',
+      active: '#1E4FFF',
+      disabled: '#1E3E99',
+    },
+    warning: {
+      normal: '#FFA010',
+      hover: '#FFAB36',
+      active: '#FF7D00',
+      disabled: '#B87610',
+    },
+    danger: {
+      normal: '#E32424',
+      hover: '#F13434',
+      active: '#D52020',
+      disabled: '#9C2424',
+    },
+  },
+};
+
+export default theme;
