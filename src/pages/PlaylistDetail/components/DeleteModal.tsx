7a46e3231cc786533a540cfa638aab0124278c1a
SongByeonghun <rokaf340@gmail.com>
2024-09-08 23:07:41 +0900

    Feat: 파이어베이스 호스팅 설정

--- src/pages/PlaylistDetail/components/DeleteModal.tsx
+++ src/pages/PlaylistDetail/components/DeleteModal.tsx
@@ -1,9 +1,5 @@
-import { styled } from 'styled-components';
-
 const DeleteModal = () => {
   return <div>DeleteModal</div>;
 };
 
 export default DeleteModal;
-
-const DeleteModalContainer = styled.div``;
